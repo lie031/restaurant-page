3cf6cf2b4a8f5c303b71be66e89ad744252f7419
lie03 <139715955+lie031@users.noreply.github.com>
2024-01-25 12:23:40 -0500

    deleted files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import aboutLoader from "./about";
 import homeLoad from "./home";
 import menuLoader from "./menu";
-import backgroudImg from './img/image.webp';
 homeLoad();
 
 const content = document.querySelector("#content")
 const menuBtn = document.querySelector('.menu-btn');
 const homeBtn = document.querySelector('.home-btn');
 const aboutBtn = document.querySelector('.about-btn')
-
-content.style.background = backgroudImg;
-
 
 homeBtn.addEventListener('click',()=>{
     content.innerHTML="";

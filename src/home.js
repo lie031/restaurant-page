065c3a67f0cbc87a08ad6ed085bc282a775e0875
lie03 <139715955+lie031@users.noreply.github.com>
2024-01-25 11:52:31 -0500

    changed files

--- src/home.js
+++ src/home.js
@@ -1,11 +1,12 @@
 import menuLoader from "./menu";
+import burger from './img/burgerImg.jpg';
 
 function homeLoad(){
     const content = document.querySelector("#content");
     const burgerImg = document.createElement("img");
     const text = document.createElement("div");
     const Btn = document.createElement("button");
-    burgerImg.src = "./dist/img/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg";
+    burgerImg.src = burger;
     burgerImg.setAttribute("width","auto");
     burgerImg.setAttribute("height","200px");
 

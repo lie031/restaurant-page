e258a6ac35c99189c92ce35b5324621e6fbff75a
lie03 <139715955+lie031@users.noreply.github.com>
2024-01-25 11:04:40 -0500

    added files

--- src/menu.js
+++ src/menu.js
@@ -4,23 +4,44 @@ export default function menuLoader(){
 
     const normal = document.createElement('div');
     const normaltitle = document.createElement("div");
-    normaltitle.textContent= "normal";
+    normaltitle.textContent= "normal - 5$";
+    normaltitle.classList.add("titlemenu");
+    const normalburger = document.createElement('img');
+    normalburger.src = "/dist/img/normalburger.png";
+    normalburger.style.width = "300px";
+    normalburger.style.height = "auto";
     normal.appendChild(normaltitle);
+    normal.appendChild(normalburger);
 
     const special = document.createElement('div');
     const specialtitle = document.createElement("div");
-    specialtitle.textContent = "special";
+    specialtitle.textContent = "special - 10$";
+    specialtitle.classList.add("titlemenu");
+    const specialburger = document.createElement('img');
+    specialburger.src = '/dist/img/specialburger.png';
+    specialburger.style.width = "300px";
+    specialburger.style.height = "auto";
     special.appendChild(specialtitle);
+    special.appendChild(specialburger);
 
     const mega = document.createElement('div');
     const megatitle = document.createElement("div");
-    megatitle.textContent = "mega";
+    megatitle.textContent = "mega - 15$";
+    megatitle.classList.add("titlemenu");
+    const megaburger = document.createElement('img');
+    megaburger.src = '/dist/img/megaburger.webp';
+    megaburger.style.width = "300px";
+    megaburger.style.height = "auto";
     mega.appendChild(megatitle);
+    mega.appendChild(megaburger);
 
 
     container.appendChild(normal);
     container.appendChild(special);
     container.appendChild(mega);
+
+    container.style.display = "flex";
+    container.style.flexWrap = "wrap";
 
     content.appendChild(container);
 }
